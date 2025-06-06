b40197a4331c548ebbacad14b26c0c77e390aef9
Vladimir <nikonov.volodymyr@gmail.com>
2025-06-06 16:43:57 +0200

    initial commit

---
+++ packages/UsrMikeApp/Schemas/UsrApplications_Detail/UsrApplications_Detail.js
@@ -0,0 +1,8 @@
+define('UsrApplications_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrApplications',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ packages/UsrMikeApp/Schemas/UsrApplications_FormPage/UsrApplications_FormPage.js
@@ -0,0 +1,480 @@
+define("UsrApplications_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "Tabs",
+				"values": {
+					"styleType": "default",
+					"mode": "tab",
+					"bodyBackgroundColor": "primary-contrast-500",
+					"selectedTabTitleColor": "auto",
+					"tabTitleColor": "auto",
+					"underlineSelectedTabColor": "auto",
+					"headerBackgroundColor": "auto"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Feed",
+				"values": {
+					"dataSourceName": "PDS",
+					"entitySchemaName": "UsrApplications"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "AttachmentList",
+				"values": {
+					"columns": [
+						{
+							"id": "26276cb0-7c59-40e1-8d11-899570710010",
+							"code": "AttachmentListDS_Name",
+							"caption": "#ResourceString(AttachmentListDS_Name)#",
+							"dataValueType": 28,
+							"width": 200
+						}
+					]
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "EntityStageProgressBar_jdr954q",
+				"values": {
+					"type": "crt.EntityStageProgressBar",
+					"saveOnChange": false,
+					"askUserToChangeSchema": true,
+					"entityName": "UsrApplications"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "UsrName",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.UsrName",
+					"control": "$UsrName",
+					"labelPosition": "auto",
+					"multiline": false
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_zgbiq30",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 2,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrAppContact_wvtiwzn",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrAppContact_wvtiwzn",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_oaa21n5",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_oaa21n5_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "ComboBox_zgbiq30",
+				"propertyName": "listActions",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_k4dozvm",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrAppOwner_ejo3nch",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrAppOwner_ejo3nch",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_a4i8e3r",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_a4i8e3r_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "ComboBox_k4dozvm",
+				"propertyName": "listActions",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_43n3b3o",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 4,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.PDS_UsrAppStatus_opo4ajh",
+					"labelPosition": "auto",
+					"control": "$PDS_UsrAppStatus_opo4ajh",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"readonly": false,
+					"placeholder": "",
+					"tooltip": "",
+					"valueDetails": null
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_701wyyp",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_701wyyp_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "ComboBox_43n3b3o",
+				"propertyName": "listActions",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "TabContainer_49zlvhp",
+				"values": {
+					"type": "crt.TabContainer",
+					"items": [],
+					"caption": "#ResourceString(TabContainer_49zlvhp_caption)#",
+					"iconPosition": "only-text",
+					"visible": true
+				},
+				"parentName": "Tabs",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "GridContainer_xh78p2s",
+				"values": {
+					"type": "crt.GridContainer",
+					"items": [],
+					"rows": "minmax(32px, max-content)",
+					"columns": [
+						"minmax(32px, 1fr)",
+						"minmax(32px, 1fr)"
+					],
+					"gap": {
+						"columnGap": "large",
+						"rowGap": 0
+					}
+				},
+				"parentName": "TabContainer_49zlvhp",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Timeline_43onypm",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.Timeline",
+					"items": [],
+					"tools": [],
+					"customFilters": [],
+					"hideTools": false,
+					"masterSchemaId": "$Id",
+					"caption": "#ResourceString(Timeline_43onypm_caption)#",
+					"label": "#ResourceString(Timeline_43onypm_label)#",
+					"filters": [],
+					"masterEntitySchemaName": "UsrApplications"
+				},
+				"parentName": "GridContainer_xh78p2s",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "TimelineTile_SysFile_cp27q43",
+				"values": {
+					"type": "crt.TimelineTile",
+					"classes": [
+						"view-element"
+					],
+					"sortedByColumn": "CreatedOn",
+					"data": {
+						"schemaType": "SysFile",
+						"isDefault": true
+					}
+				},
+				"parentName": "Timeline_43onypm",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "TimelineTile_Feed_cfizb05",
+				"values": {
+					"type": "crt.TimelineTile",
+					"classes": [
+						"view-element"
+					],
+					"sortedByColumn": "CreatedOn",
+					"data": {
+						"schemaType": "Feed",
+						"isDefault": true
+					}
+				},
+				"parentName": "Timeline_43onypm",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "MessageComposerSelector_n9mzov2",
+				"values": {
+					"type": "crt.MessageComposerSelector",
+					"items": [],
+					"classes": [
+						"view-element"
+					]
+				},
+				"parentName": "Timeline_43onypm",
+				"propertyName": "tools",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "FeedComposer_2umd61o",
+				"values": {
+					"type": "crt.FeedComposer",
+					"classes": [
+						"view-element"
+					],
+					"sortedByColumn": "CreatedOn",
+					"data": {
+						"uId": "6d006667-3496-4e2d-adc0-3a42648dd97b",
+						"schemaType": "Feed",
+						"caption": "Feed",
+						"sortedByColumn": "CreatedOn",
+						"typeName": "crt.FeedComposer",
+						"icon": "feed-composer-icon"
+					},
+					"feedType": "Record",
+					"primaryColumnValue": "$Id",
+					"cardState": "$CardState",
+					"entitySchemaName": "UsrApplications",
+					"dataSourceName": "PDS"
+				},
+				"parentName": "MessageComposerSelector_n9mzov2",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "EmailComposer_xc16ybi",
+				"values": {
+					"type": "crt.EmailComposer",
+					"classes": [
+						"view-element"
+					],
+					"sortedByColumn": "CreatedOn",
+					"data": {
+						"uId": "75aadc65-a834-42d0-b880-fac9bdee4c86",
+						"schemaType": "Email",
+						"caption": "Email",
+						"sortedByColumn": "CreatedOn",
+						"typeName": "crt.EmailComposer",
+						"icon": "email-composer-icon"
+					},
+					"recordId": "$Id",
+					"defaultSenderRequest": "crt.DefaultSenderComposerRequest",
+					"entitySchemaName": "UsrApplications"
+				},
+				"parentName": "MessageComposerSelector_n9mzov2",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Timeline_43onypm_TimelineFilter_Entity",
+				"values": {
+					"type": "TimelineFilter_Entity",
+					"visible": true
+				},
+				"parentName": "Timeline_43onypm",
+				"propertyName": "filters",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Timeline_43onypm_TimelineFilter_Date",
+				"values": {
+					"type": "TimelineFilter_Date",
+					"visible": true
+				},
+				"parentName": "Timeline_43onypm",
+				"propertyName": "filters",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Timeline_43onypm_TimelineFilter_Owner",
+				"values": {
+					"type": "TimelineFilter_Owner",
+					"visible": true
+				},
+				"parentName": "Timeline_43onypm",
+				"propertyName": "filters",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "Timeline_43onypm_TimelineFilter_SystemMessages",
+				"values": {
+					"type": "TimelineFilter_SystemMessages",
+					"visible": true
+				},
+				"parentName": "Timeline_43onypm",
+				"propertyName": "filters",
+				"index": 3
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"UsrName": {
+						"modelConfig": {
+							"path": "PDS.UsrName"
+						}
+					},
+					"PDS_UsrAppContact_wvtiwzn": {
+						"modelConfig": {
+							"path": "PDS.UsrAppContact"
+						}
+					},
+					"PDS_UsrAppOwner_ejo3nch": {
+						"modelConfig": {
+							"path": "PDS.UsrAppOwner"
+						}
+					},
+					"PDS_UsrAppStatus_opo4ajh": {
+						"modelConfig": {
+							"path": "PDS.UsrAppStatus"
+						}
+					}
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"attributes",
+					"Id",
+					"modelConfig"
+				],
+				"values": {
+					"path": "PDS.Id"
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"primaryDataSourceName": "PDS"
+				}
+			},
+			{
+				"operation": "merge",
+				"path": [
+					"dataSources"
+				],
+				"values": {
+					"PDS": {
+						"type": "crt.EntityDataSource",
+						"config": {
+							"entitySchemaName": "UsrApplications"
+						},
+						"scope": "page"
+					}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
